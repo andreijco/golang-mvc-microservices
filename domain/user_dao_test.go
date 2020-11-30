c44d053e092db5277e1c066cff753c3ed89f9a2c
Andrew Limaru <3809608+andreijco@users.noreply.github.com>
2020-11-30 13:31:21 +1100

    modify the test case to use testify/assert library

--- domain/user_dao_test.go
+++ domain/user_dao_test.go
@@ -3,20 +3,16 @@ package domain
 import (
 	"net/http"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetUserNoUserFound(t *testing.T) {
 	user, err := GetUser(0)
 
-	if user != nil {
-		t.Error("we were not expecting a user with id 0")
-	}
-
-	if err == nil {
-		t.Error("we were expecting an error when user id is 0")
-	}
-
-	if err.StatusCode != http.StatusNotFound {
-		t.Error("we were expecting 404 when user is not found")
-	}
+	assert.Nil(t, user, "we were not expecting a user with id 0")
+	assert.NotNil(t, err, "we were expecting an error when user id is 0")
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode, "we were expecting 404 when user is not found")
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 does not exist", err.Message)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module golang/micro
 
 go 1.14
+
+require github.com/stretchr/testify v1.6.1
